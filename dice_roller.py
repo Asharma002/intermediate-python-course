0a7d44cf0d361917c3dda3efd55d0e1f391a3841
Asharma002 <79785668+Asharma002@users.noreply.github.com>
2021-02-28 18:03:53 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,6 @@
-def main():
-  #print('You rolled a die')
+print('You rolled a die and output is:',x)
+import random
+dice_options =[1,2,3,4,5,6,]
+x = random.choice(dice_options)
+print(x)
 
-if __name__== "__main__":
-  main()
